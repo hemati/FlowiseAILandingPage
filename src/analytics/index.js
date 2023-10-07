e516d5895b1c66d66144fa20f210e2e1a4c0607f
Wahed Hemati <wahed.hemati@shikenso.com>
2023-10-07 14:43:43 +0200

    Disable Google Analytics tracking
    The Google Analytics tracking in the "analytics/index.js" file has been commented out. This change was made to prevent data collection during development and testing phases. To re-enable tracking, uncomment the relevant lines of code.

--- src/analytics/index.js
+++ src/analytics/index.js
@@ -2,26 +2,26 @@ import ReactGA from 'react-ga4';
 
 export const initGA = () => {
   console.log('GA init');
-  ReactGA.initialize('G-RJXVX78LLH');
+  // ReactGA.initialize('G-RJXVX78LLH');
 };
 
 export const logPageView = () => {
   console.log(`Logging pageview for ${window.location.pathname}`);
-  ReactGA.send({ hitType: "pageview", page: window.location.pathname, title: document.title });
+  // ReactGA.send({ hitType: "pageview", page: window.location.pathname, title: document.title });
 };
 
 export const logEvent = (category = '', action = '') => {
-  if (category && action) {
-    ReactGA.event({
-      category: category,
-      action: action,
-      nonInteraction: true
-    });
-  }
+  // if (category && action) {
+  //   ReactGA.event({
+  //     category: category,
+  //     action: action,
+  //     nonInteraction: true
+  //   });
+  // }
 };
 
 export const logException = (description = '', fatal = false) => {
-  if (description) {
-    ReactGA.exception({ description, fatal });
-  }
+  // if (description) {
+  //   ReactGA.exception({ description, fatal });
+  // }
 };
